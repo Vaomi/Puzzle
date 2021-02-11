1be8e1d9cc8b3c512fc896869c2ae05e84e5931b
Vadim Rumyantsev <opavadimka@live.com>
2021-02-11 13:26:02 +0300

    Добавьте файлы проекта.

---
+++ Puzzle/Forms/Login.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Windows.Media.Animation;
+
+
+namespace Puzzle
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+            /*DoubleAnimation buttonAnimation = new DoubleAnimation();
+            //buttonAnimation.From = helloButton.ActualWidth;
+            buttonAnimation.From = 75;
+            buttonAnimation.To = 150;
+            buttonAnimation.SpeedRatio = 2;
+            buttonAnimation.Duration = TimeSpan.FromSeconds(3);
+            helloButton.BeginAnimation(System.Windows.Controls.Button.WidthProperty, buttonAnimation);
+            */
+        }
+
+        private void helloButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+
+            //Закрытие Login и открытие MainMenu
+            MainMenu Mm = new MainMenu();
+            Close();
+            Mm.Show();
+            
+        }
+    }
+}
---
+++ Puzzle/Forms/MainMenu.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Puzzle
+{
+    /// <summary>
+    /// Логика взаимодействия для MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void StartGame_Click(object sender, RoutedEventArgs e)
+        {
+            ThemePuzzle TP = new ThemePuzzle();
+            TP.ShowDialog();
+        }
+
+        private void Records_Click(object sender, RoutedEventArgs e)
+        {
+            Records rec = new Records();
+            rec.ShowDialog();
+        }
+
+        private void AboutTheProgram_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
